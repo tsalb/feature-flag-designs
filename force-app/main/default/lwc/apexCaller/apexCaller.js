b0e1025a11f306ac08d8b5e72446feea9c440cd2
James Hou <james@sparkworks.io>
2020-04-09 17:47:05 -0700

    Working example on Account Flexipage

--- force-app/main/default/lwc/apexCaller/apexCaller.js
+++ force-app/main/default/lwc/apexCaller/apexCaller.js
@@ -2,10 +2,11 @@ import { LightningElement, api } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 // Static list of all variations, we just want something easy for demoing
-import getDataFromSomeClass from '@salesforce/apex/SomeClass.getData';
-import getDataFromSomeOtherClass from '@salesforce/apex/SomeOtherClass.getData';
+import getDataSimple from '@salesforce/apex/SomeClass.getDataSimple';
+import getDataComplex from '@salesforce/apex/SomeOtherClass.getDataComplex';
 
 export default class ApexCaller extends LightningElement {
+    @api recordId;
     @api apexClassName;
 
     async handleClick() {
@@ -14,10 +15,10 @@ export default class ApexCaller extends LightningElement {
         if (this.apexClassName) {
             switch (this.apexClassName) {
                 case 'SomeClass':
-                    message = await getDataFromSomeClass();
+                    message = await getDataSimple();
                     break;
                 case 'SomeOtherClass':
-                    message = await getDataFromSomeOtherClass();
+                    message = await getDataComplex({ recordId: this.recordId });
                     break;
                 default:
                     // Nothing
